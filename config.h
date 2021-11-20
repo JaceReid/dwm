a8694bc21efac4cf6ea9de7b39fc6f69b322038e
JamesDevine42 <jamescdevine@gmail.com>
2021-11-20 16:05:21 +0200

    16:05  11_20_2021 Update

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //tag names (upper left)
-static const char *tags[] = { "", "", "", "", "",  "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "",  "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
