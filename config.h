9c70ccca02010e6a20dee22c9951943a29f23e72
JaceReid <jamescdevine@gmail.com>
2022-01-27 07:16:04 +0200

    Only shows tags with active windows

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //tag names (upper left)
-static const char *tags[] = { "1", "2", "3", "4", "5",  "6", "7", "8", "9" };
+static const char *tags[] = { "", "2", "3", "4", "5",  "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
